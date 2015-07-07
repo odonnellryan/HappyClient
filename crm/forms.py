d9fa43368b59ba26918198866ff0c4b81877c068
odonnellryan <odonnellryanc@gmail.com>
2015-07-07 18:48:01 -0400

    changes some forms

--- crm/forms.py
+++ crm/forms.py
@@ -1,16 +1,17 @@
 from utilities import helper_functions
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField, DateTimeField
 from wtforms.validators import DataRequired
 
 
-class ClientForm(helper_functions.RedirectForm):
+class NewClientForm(helper_functions.RedirectForm):
     name = StringField('Name', validators=[DataRequired()])
     contact_information = TextAreaField('Contact Information')
     location = StringField('Location')
+    interaction_reminder_time = DateTimeField('Set contact reminder')
+    interaction_reminder_notes = TextAreaField('Add notes regarding contact reminder')
     notes = TextAreaField('Notes')
 
 class CompanyForm(helper_functions.RedirectForm):
     name = StringField('Name', validators=[DataRequired()])
-    contact_information = TextAreaField('Contact Information')
-    location = StringField('Location')
-    notes = TextAreaField('Notes')
+    phone_number = TextAreaField('Phone Number')
+    address = StringField('Address')
